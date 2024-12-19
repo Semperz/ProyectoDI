e61f3f2c915daf5fe11e40084a7add1379125ede
a23sergiopg <serprigar@gmail.com>
2024-12-19 10:41:09 +0100

    feat(informes.py): add informes

--- informes.py
+++ informes.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+from io import BytesIO
 
+from PyQt6.QtGui import QPixmap
+from svglib.svglib import svg2rlg
 from reportlab.pdfgen import canvas
+from reportlab.graphics import renderPM
 import os, shutil
 import var
 from PIL import Image
@@ -8,31 +12,32 @@ from PIL import Image
 class Informes:
 
     @staticmethod
-    def reportClientes(self):
+    def reportClientes():
         try:
+            rootPath = '.\\informes'
+            if not os.path.exists(rootPath):
+                os.makedirs(rootPath)
             fecha = datetime.today()
             fecha = fecha.strftime("%Y_%m_%d_%H_%M_%S")
             nompdfcli = fecha + "_listadoclientes.pdf"
-            var.report = canvas.Canvas('informes/', nompdfcli)
+            pdf_path = os.path.join(rootPath, nompdfcli)
+            var.report = canvas.Canvas(pdf_path)
             titulo = "Listado clientes"
             Informes.topInforme(titulo)
             Informes.footInforme(titulo)
             items = ['DNI', 'APELLIDOS', 'NOMBRE', 'MOVIL', 'PROVINCIA', 'MUNICIPIO']
             var.report.setFont('Helvetica-Bold', size=10)
-            var.report.drawString(50, 650, str(items[0]))
-            var.report.drawString(120, 650, str(items[1]))
-            var.report.drawString(170, 650, str(items[2]))
+            var.report.drawString(55, 650, str(items[0]))
+            var.report.drawString(100, 650, str(items[1]))
+            var.report.drawString(190, 650, str(items[2]))
             var.report.drawString(285, 650, str(items[3]))
-            var.report.drawString(390, 650, str(items[4]))
-            var.report.drawString(460, 650, str(items[5]))
+            var.report.drawString(360, 650, str(items[4]))
+            var.report.drawString(450, 650, str(items[5]))
             var.report.line(50, 645, 525, 645)
             var.report.save()
-
-            rootPath = '.\\informes'
-
-            for file in os.endswith(rootPath):
+            for file in os.listdir(rootPath):
                 if file.endswith(nompdfcli):
-                    os.startfile(file) ('%s\\%' % (rootPath, file))
+                    os.startfile(pdf_path)
 
 
         except Exception as e:
@@ -54,8 +59,15 @@ class Informes:
 
     def topInforme(titulo):
         try:
-            ruta_logo = '.\\img\\icono.svg'
-            logo = Image.open(ruta_logo)
+            ruta_logo_svg = '.\\img\\icono.svg'
+            ruta_logo_png = '.\\img\\icono.png'
+
+            # Convertir SVG a PNG
+            logo = svg2rlg(ruta_logo_svg)
+            renderPM.drawToFile(logo, ruta_logo_png, fmt='PNG')
+
+            # Cargar la imagen PNG
+            logo = Image.open(ruta_logo_png)
 
             # Asegúrate de que el objeto 'logo' sea de tipo 'PngImageFile'
             if isinstance(logo, Image.Image):
@@ -66,7 +78,7 @@ class Informes:
                 var.report.line(50, 665, 525, 665)
 
                 # Dibuja la imagen en el informe
-                var.report.drawImage(ruta_logo, 480, 725, width=40, height=40)
+                var.report.drawImage(ruta_logo_png, 480, 725, width=40, height=40)
 
                 var.report.setFont('Helvetica', size=9)
                 var.report.drawString(55, 770, 'CIF: A12345678')
@@ -75,6 +87,7 @@ class Informes:
                 var.report.drawString(55, 725, 'Teléfono: 986 132 456')
                 var.report.drawString(55, 710, 'e-mail: cartesteisr@mail.com')
             else:
-                print(f'Error: No se pudo cargar la imagen en {ruta_logo}')
+                print(f'Error: No se pudo cargar la imagen en {ruta_logo_png}')
         except Exception as error:
             print('Error en cabecera informe:', error)
+
